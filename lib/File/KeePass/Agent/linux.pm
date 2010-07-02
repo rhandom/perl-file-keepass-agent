5c4c3683874cf5129d86cc33b5d9c4dc59b9d4b8
paul <paul>
2010-07-02 14:03:56 +0000

    add perldoc

--- lib/File/KeePass/Agent/linux.pm
+++ lib/File/KeePass/Agent/linux.pm
@@ -10,3 +10,20 @@ use File::KeePass::Agent::unix;
 use base qw(File::KeePass::Agent::unix);
 
 1;
+
+__END__
+
+=head1 DESCRIPTION
+
+This module linux based support for the File::KeePassAgent.  It should
+not normally be used on its own.
+
+=head1 AUTHOR
+
+Paul Seamons <paul at seamons dot com>
+
+=head1 LICENSE
+
+This module may be distributed under the same terms as Perl itself.
+
+=cut
